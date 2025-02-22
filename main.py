298bd8ffe6ebf5f2d7ced041159d9b23d1195a6f
iii123iii <euyhegnrh@gmail.com>
2025-02-22 10:08:08 +0200

    grouped stocks

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from yfinance import Tickers
 stocks = [["AAPL", 2, 84.26], ["AAPL", 3, 245.55], ["GOOGL", 4, 45]]
 owned_stocks = []
 
-
 owned_stocks = list(set(stock[0] for stock in stocks))
 
 
@@ -14,3 +13,13 @@ stocks_data_map = {}
 
 for name, value in stocks_data.items():
     stocks_data_map[name] = value.info['currentPrice']
+
+grouped_stocks = {}
+
+for value in stocks:
+    if value[0] not in grouped_stocks:
+        grouped_stocks[value[0]] = [value]
+    else:
+        grouped_stocks[value[0]].append(value)
+
+print(grouped_stocks)
