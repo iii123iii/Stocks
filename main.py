ce03cd9f520cb02164e45d925de3854a5da7fd41
iii123iii <euyhegnrh@gmail.com>
2025-02-22 11:12:40 +0200

    Added the prints for all the stocks

--- main.py
+++ main.py
@@ -21,3 +21,21 @@ for value in stocks:
         grouped_stocks[value[0]] = [value]
     else:
         grouped_stocks[value[0]].append(value)
+
+for key, value in grouped_stocks.items():
+    for stock in value:
+        border = "•" * 40
+        print("\n" + border)
+        print(f"{'📈 Stock name:':<25} {stock[0]}")
+        print(f"{'🔢 Quantity:':<25} {stock[1]}")
+        print(f"{'💵 Purchase price:':<25} ${stock[2]:.2f}")
+        print(f"{'💰 Current price:':<25} ${stocks_data_map[stock[0]]:.2f}")
+        print(f"{'💎 Holdings:':<25} ${stocks_data_map[stock[0]] * stock[1]:.2f}")
+        
+        profit = stocks_data_map[stock[0]] * stock[1] - stock[2] * stock[1]
+        profit_percentage = ((stocks_data_map[stock[0]] - stock[2]) / stock[2]) * 100
+        
+        profit_emoji = "📈" if profit > 0 else "📉"
+        print(f"{profit_emoji + ' Profit:':<25} ${profit:.2f}")
+        print(f"{profit_emoji + ' Profit %:':<25} {profit_percentage:.2f}%")
+        print(border + "\n")
